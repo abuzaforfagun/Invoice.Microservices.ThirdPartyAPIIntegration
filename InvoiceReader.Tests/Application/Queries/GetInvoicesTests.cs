c06daf842fc76302d2bb57f5b1c76af77265fb1a
Abu Zafor Fagun <mdabuzaforfagun@gmail.com>
2021-09-22 23:05:26 +0600

    Issue-13 remove dead code relates to Distributed Cache

--- InvoiceReader.Tests/Application/Queries/GetInvoicesTests.cs
+++ InvoiceReader.Tests/Application/Queries/GetInvoicesTests.cs
@@ -1,13 +1,8 @@
 ﻿using Integration.Likvido;
-using InvoiceReader.Application;
 using InvoiceReader.Application.Queries.GetInvoices;
-using InvoiceReader.Domain.Entity;
-using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
 using Moq;
-using Newtonsoft.Json;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -17,38 +12,19 @@ namespace InvoiceReader.Tests.Application.Queries
     public class GetInvoicesTests
     {
         private readonly Mock<ILikvidoClient> _clientMock;
-        private readonly MemoryDistributedCache _cache;
 
         public GetInvoicesTests()
         {
             _clientMock = new Mock<ILikvidoClient>();
 
             var opts = Options.Create(new MemoryDistributedCacheOptions());
-            _cache = new MemoryDistributedCache(opts);
         }
 
         [Fact]
-        public async Task Get_Invoices_From_Cache_When_Available()
+        public async Task Get_Invoices_From_Client()
         {
             // Arrange
-            var invoices = new List<Invoice>();
-            var emptyResult = new GetInvoices.Result(invoices);
-            var jsonResult = JsonConvert.SerializeObject(emptyResult);
-            await _cache.SetStringAsync(Constants.GetInvoicesKey, jsonResult);
-            var handler = new GetInvoices.QueryHandler(_clientMock.Object, _cache);
-
-            // Act
-            await handler.Handle(new GetInvoices.Query(), new CancellationToken());
-
-            // Assert
-            _clientMock.Verify(m => m.GetInvoicesAsync(), Times.Never);
-        }
-
-        [Fact]
-        public async Task Get_Invoices_From_Client_When_No_Cache_Available()
-        {
-            // Arrange
-            var handler = new GetInvoices.QueryHandler(_clientMock.Object, _cache);
+            var handler = new GetInvoices.QueryHandler(_clientMock.Object);
 
             // Act
             await handler.Handle(new GetInvoices.Query(), new CancellationToken());
