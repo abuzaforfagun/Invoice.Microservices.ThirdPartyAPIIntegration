eb7233e6763d5b17c4ab6a4c3d6f34a0b8cf2486
Yeasin Arafat <yeasinarafat10@gmail.com>
2021-09-27 13:21:43 +0600

    Add unit test for OutBoxService

---
+++ InvoiceProcessor.Tests/Infrastructure/Outbox/Services/OutBoxServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using InvoiceProcessor.Domain.Enums;
+using InvoiceProcessor.Domain.Events;
+using InvoiceProcessor.Domain.Interfaces.Outbox;
+using InvoiceProcessor.Infrastructure.Outbox;
+using InvoiceProcessor.Infrastructure.Outbox.Services;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace InvoiceProcessor.Tests.Infrastructure.Outbox.Services
+{
+    public class OutBoxServiceTests
+    {
+        private readonly Mock<IOutboxStorage> _storageMock;
+        private readonly OutBoxService _outBoxServiceMock;
+
+        public OutBoxServiceTests()
+        {
+            _storageMock = new Mock<IOutboxStorage>();
+            _outBoxServiceMock = new OutBoxService(_storageMock.Object);
+        }
+
+        [Fact]
+        public async Task Should_Upsert()
+        {
+            // Assign 
+            var guid = Guid.NewGuid();
+            var payload = new OutBoxEvent(guid);
+            var model = new OutBoxModel(payload, OutBoxStatus.Pending, "abcd");
+
+            // Act
+            await _outBoxServiceMock.Upsert(model);
+
+            // Assert
+            _storageMock.Verify();
+        }
+    }
+}
